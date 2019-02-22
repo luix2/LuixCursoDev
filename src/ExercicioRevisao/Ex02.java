66af80eb5d3ba9282ece76e287c6ad9e562a33ec
04032329 <filhoxinho@hotmail.com>
2019-02-21 21:05:23 -0300

    Exercício 02

--- src/ExercicioRevisao/Ex02.java
+++ src/ExercicioRevisao/Ex02.java
@@ -1,7 +1,15 @@
 package ExercicioRevisao;
 
 public class Ex02 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int totalFolha = 105;
+        double Resultado;
+        if (totalFolha <= 100) {
+            Resultado = totalFolha * 0.25;
+        } else {
+            Resultado = totalFolha * 0.20;
 
+        }
+        System.out.println ( Resultado );
     }
 }
