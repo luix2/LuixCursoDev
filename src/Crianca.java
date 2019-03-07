62cebb2bd30ec8525cba174f03f6ee10bc3ad383
04032329 <filhoxinho@hotmail.com>
2019-03-07 20:40:26 -0300

    aula 07/03/2019

---
+++ src/Crianca.java
@@ -0,0 +1,14 @@
+public class Crianca {
+    String corCabelo;
+    String corPele;
+    double altura;
+    int idade;
+
+    //atributos omitidos
+    void brincar(){
+        System.out.println("Brincando...");
+    }
+    void correr(double metros){
+        System.out.println("Correndo " + metros + " metros");
+    }
+}
