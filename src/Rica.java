5e91d3c328dad2c2ccb64e3fe55dc1145832bbdf
04032329 <filhoxinho@hotmail.com>
2019-04-29 20:58:28 -0300

    aula2 21/03/2019 28/03/2019 aula 28/03/2019 aula2
    private privando cpf.
    
    Aula - 29/04/2019
    aula - 29/04/2019

---
+++ src/Rica.java
@@ -0,0 +1,11 @@
+public class Rica extends Pessoa {
+    private double dinheiro;
+
+    public double getDinheiro() {
+        return dinheiro;
+    }
+
+    public void setDinheiro(double dinheiro) {
+        this.dinheiro = dinheiro;
+    }
+}
